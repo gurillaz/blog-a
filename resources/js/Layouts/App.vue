4c0cd974cf3d724c28d8cb4c55141aaab254c534
unknown <klevissahitaj@github.com>
2019-12-04 17:03:09 +0100

    Background color.

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -1,9 +1,20 @@
 
 <template>
+<v-app>
 
-                    <router-view></router-view>
+  <router-view></router-view>
 
+</v-app>
 </template>
 
 <script>
 </script>
+
+
+<style lang="css">
+#app {
+    /* background-color: aqua; */
+  /* color:# ; */
+  background-color: rgba(0, 0, 0, 0.03);  
+}
+</style>
